5c8659955ecce40a4916ec8814688012160e08c2
akshatminocha <akshatminocha@gmail.com>
2020-02-25 22:57:15 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import { EmptyState, Layout, Page } from '@shopify/polaris';
 import { ResourcePicker, TitleBar } from '@shopify/app-bridge-react';
 import store from 'store-js';
 import ResourceListWithProducts from '../components/ResourceList';
-import FetchProduct from '../components/FetchRender';
+//  import FetchProduct from '../components/FetchRender';
 //import Axios from '../components/Product-1';
 
 const img = 'https://cdn.shopify.com/s/files/1/0757/9955/files/empty-state.svg';
@@ -14,9 +14,9 @@ class Index extends React.Component {
     return (
       <Page>
       <h1> List of Products </h1>
-      <div>
-        <FetchProduct />
-      </div>
+//       <div>
+//         <FetchProduct />
+//       </div>
         <TitleBar
           primaryAction={{
             content: 'Select products',
